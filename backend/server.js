ec8913af8a813c13a0b21aef5b6df74644eda7e9
abhinildev <savarniabhinil@gmail.com>
2025-12-18 17:26:34 +0530

    Deployment 9

--- backend/server.js
+++ backend/server.js
@@ -39,7 +39,7 @@ app.use("/timeslots", timeSlotRouter);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "frontend/dist")));
 
-  app.get("/*", (req, res) => {
+  app.use( (req, res) => {
     res.sendFile(path.join(__dirname, "frontend/dist", "index.html"));
   });
 }

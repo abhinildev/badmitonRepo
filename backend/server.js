567ecf208227f236c3b7a09a4d878bc396771caf
abhinildev <savarniabhinil@gmail.com>
2025-12-18 17:23:24 +0530

    Deployment 8

--- backend/server.js
+++ backend/server.js
@@ -1,47 +1,55 @@
-import express from "express"
-import dotenv from "dotenv"
-import cors from "cors"
-import dbconnection from "./src/config/db.js"
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import path from "path";
+
+import dbconnection from "./src/config/db.js";
 import authRouter from "./src/router/auth.route.js";
 import adminRouter from "./src/router/admin.router.js";
 import bookingRouter from "./src/router/booking.route.js";
 import pricingRouter from "./src/router/pricingRoutes.js";
 import timeSlotRouter from "./src/router/timeSlotRoutes.js";
-import TimeSlot from "./src/model/time_slots.js"
-//import bookingRouter from "./src/router/booking.route.js"
-import path from "path"
-import "./src/model/associations.js"
-import { fileURLToPath } from "url";
-const app =express()
-dotenv.config()
 
-const port=process.env.PORT;
-app.use(express.json())
-app.use(cors({
-    origin:"http://localhost:5173",
-    credentials: true
-}))
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+import "./src/model/associations.js";
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "public")));
+dotenv.config();
 
-  app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "public", "index.html"));
-  });
-}
-//routes
+const app = express();
+const __dirname = path.resolve();
+const PORT = process.env.PORT || 7000;
+
+/* ---------------- middleware ---------------- */
+app.use(express.json());
+
+app.use(
+  cors({
+    origin: true,
+    credentials: true,
+  })
+);
+
 
 app.use("/auth", authRouter);
 app.use("/admin", adminRouter);
 app.use("/bookings", bookingRouter);
 app.use("/pricing", pricingRouter);
 app.use("/timeslots", timeSlotRouter);
+
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "frontend/dist")));
+
+  app.get("/*", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend/dist", "index.html"));
+  });
+}
+
+
 dbconnection
-    .sync({alter:true})
-    .then(()=>console.log("Neon is connected"))
-    .catch((err)=>console.error("DB connection error"))
-app.listen(port ,()=>{
-    console.log("Server is running on port: "+port)
-})
+  .sync()
+  .then(() => console.log("Neon is connected"))
+  .catch((err) => console.error("DB connection error:", err));
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});

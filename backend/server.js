a328ae490460303b8d5613d2d95ddff8af3e95aa
abhinildev <savarniabhinil@gmail.com>
2025-12-18 16:08:40 +0530

    Deployment 7

--- backend/server.js
+++ backend/server.js
@@ -27,7 +27,7 @@ const __dirname = path.dirname(__filename);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "public")));
 
-  app.get("*", (req, res) => {
+  app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "public", "index.html"));
   });
 }
